9d5be6e0c125afc01bb30407c7a49d4b55c32c1a
alexebarlescu <alexebarlescu@gmailcom>
2021-03-08 17:18:34 +0000

    Blazor Form Validation

--- Web/Models/Employee.cs
+++ Web/Models/Employee.cs
@@ -1,10 +1,21 @@
-﻿namespace Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Employee full name is required.")]
+        [MinLength(3, ErrorMessage = "Name should be 3 or more characters.")]
         public string Name { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Select employee department.")]
         public int DepartmentId { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Select employee location.")]
         public int LocationId { get; set; }
 
         public Department Department { get; set; }
